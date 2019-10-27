b9f9d884da3c07d7a7b713a9c6db9eee5a2022ec
jonahcoleman23 <jonahcoleman23@gmail.com>
2019-10-27 23:54:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function logShout(string){
 function logWhisper(string){
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string){
+  var lowerCase = string
+  if (string.toLowerCase()===lowerCase){
+    return console.log('I can\'t hear you!')
+  }
+}
